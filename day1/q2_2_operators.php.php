5f4f6a1928711b0f860b689ffd2a78577053009f
Nikhil Mathapati <mathapatiniksh18ce@student.mes.ac.in>
2020-10-01 12:18:42 +0530

    first commit

---
+++ day1/q2_2_operators.php.php
@@ -0,0 +1,10 @@
+<?php
+$num1=4;
+$num2=5;
+if($num1==$num2)
+	echo " The numbers are equal";
+elseif($num1>$num2) 
+	echo " num1 is greater than num2";
+else
+	echo "num 1 is smaller than num2";
+?>
